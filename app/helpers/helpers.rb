a55dd8274fed9a7c5ff37d4fabd679a47ec1f1ed
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-11-09 12:20:28 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def self.current_user(sessions)
+    #binding.pry
+    User.find_by_id(sessions[:user_id])
+  end
+
+  def self.is_logged_in?(sessions)
+    !! sessions[:user_id]
+  end
 end
